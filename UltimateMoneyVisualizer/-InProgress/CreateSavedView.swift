712a77e005d57c0a1b1932713fc5bc6c6f517f4d
vdeaugustine <vinnied44@csu.fullerton.edu>
2023-04-26 00:53:06 -0700

    create saved item and view it

---
+++ UltimateMoneyVisualizer/-InProgress/CreateSavedView.swift
@@ -0,0 +1,131 @@
+//
+//  CreateSavedView.swift
+//  UltimateMoneyVisualizer
+//
+//  Created by Vincent DeAugustine on 4/26/23.
+//
+
+import AlertToast
+import SwiftUI
+
+// MARK: - CreateSavedView
+
+struct CreateSavedView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+
+    @State private var title: String = "Didn't go to lunch"
+    @State private var amount: String = "10"
+    @State private var info: String = "Ate at home"
+    @State private var date: Date = Date()
+
+    // Alert toast state variables
+    @State private var showToast = false
+    @State private var alertToastConfig = AlertToast(displayMode: .hud, type: .regular, title: "")
+
+    var body: some View {
+        Form {
+            Section(header: Text("Saved Information")) {
+                TextField("Title", text: $title)
+                TextField("Amount", text: $amount)
+                    .keyboardType(.decimalPad)
+                TextField("Info", text: $info)
+                DatePicker("Date", selection: $date, displayedComponents: .date)
+            }
+
+            Section {
+                Button("Save") {
+                    // Create a new Saved object and save it to Core Data
+
+                    guard !title.isEmpty else {
+                        alertToastConfig = AlertToast(displayMode: .alert, type: .error(.blue), title: "Title must not be empty")
+                        showToast = true
+                        return
+                    }
+                    
+                    guard let dub = Double(amount) else {
+                        alertToastConfig = AlertToast(displayMode: .alert, type: .error(.blue), title: "Please enter a valid amount")
+                        showToast = true
+                        return
+                    }
+
+                    do {
+                        let saved = Saved(context: viewContext)
+                        saved.title = title
+                        saved.amount = dub
+                        saved.info = info
+                        saved.date = date
+
+                        try viewContext.save()
+
+                        // Reset the fields
+                        title = ""
+                        amount = "0.0"
+                        info = ""
+                        date = Date()
+
+                        // Show a success alert toast
+                        alertToastConfig.title = "Item saved successfully."
+                        showToast = true
+                    } catch let error {
+                        // Show an error alert toast
+                        alertToastConfig.title = "Error: \(error.localizedDescription)"
+                        showToast = true
+                    }
+                }
+            }
+        }
+        .navigationTitle("New Saved Item")
+        // Add the alert toast modifier to the view
+        .toast(isPresenting: $showToast, duration: 2, tapToDismiss: true) {
+            alertToastConfig
+        } onTap: {
+            showToast = false
+        }
+        .toolbar {
+            ToolbarItem {
+                NavigationLink("Saved Items") {
+                    SavedListView()
+                }
+            }
+        }
+    }
+}
+
+// MARK: - CreateSavedView_Previews
+
+struct CreateSavedView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateSavedView()
+            .environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    }
+}
+
+// MARK: - CurrencyFormatter
+
+//struct CurrencyFormatter: Formatter {
+//    let numberFormatter: NumberFormatter = {
+//        let nf = NumberFormatter()
+//        nf.numberStyle = .currency
+//        nf.currencySymbol = "$"
+//        return nf
+//    }()
+//
+//    func string(for value: Any?) -> String? {
+//        guard let value = value as? Double else { return nil }
+//        return numberFormatter.string(from: NSNumber(value: value))
+//    }
+//
+//    func getObjectValue(_ obj: AutoreleasingUnsafeMutablePointer<AnyObject?>?, for string: String, errorDescription error: AutoreleasingUnsafeMutablePointer<NSString?>?) -> Bool {
+//        guard let number = numberFormatter.number(from: string) else { return false }
+//        obj?.pointee = number.doubleValue as AnyObject
+//        return true
+//    }
+//}
+
+// MARK: - CreateSavedView_Previews
+
+//struct CreateSavedView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        CreateSavedView()
+//    }
+//}
---
+++ UltimateMoneyVisualizer/-InProgress/SavedListView.swift
@@ -0,0 +1,84 @@
+//
+//  SavedListView.swift
+//  UltimateMoneyVisualizer
+//
+//  Created by Vincent DeAugustine on 4/26/23.
+//
+
+import SwiftUI
+
+struct SavedListView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Saved.date, ascending: false)],
+        animation: .default)
+    private var savedItems: FetchedResults<Saved>
+    
+    var body: some View {
+            List {
+                ForEach(savedItems, id: \.self) { saved in
+                    NavigationLink(destination: SavedDetailView(saved: saved)) {
+                        VStack(alignment: .leading) {
+                            Text(saved.title ?? "")
+                                .font(.headline)
+                            Text(saved.info ?? "")
+                                .font(.subheadline)
+                                .foregroundColor(.gray)
+                        }
+                    }
+                }
+                .onDelete(perform: deleteSavedItems)
+            }
+            .navigationTitle("Saved Items")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    EditButton()
+                }
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button(action: addSavedItem) {
+                        Image(systemName: "plus")
+                    }
+                }
+            }
+        
+    }
+    
+    private func addSavedItem() {
+        withAnimation {
+            let newSavedItem = Saved(context: viewContext)
+            newSavedItem.date = Date()
+            do {
+                try viewContext.save()
+            } catch {
+                print("Error saving new saved item: \(error)")
+            }
+        }
+    }
+    
+    private func deleteSavedItems(offsets: IndexSet) {
+        withAnimation {
+            offsets.map { savedItems[$0] }.forEach(viewContext.delete)
+            do {
+                try viewContext.save()
+            } catch {
+                print("Error deleting saved items: \(error)")
+            }
+        }
+    }
+}
+
+struct SavedListView_Previews: PreviewProvider {
+    static var previews: some View {
+        let context = PersistenceController.preview.container.viewContext
+        let savedItem1 = Saved(context: context)
+        savedItem1.title = "Saved Item 1"
+        savedItem1.date = Date()
+        let savedItem2 = Saved(context: context)
+        savedItem2.title = "Saved Item 2"
+        savedItem2.date = Date()
+        return SavedListView()
+            .environment(\.managedObjectContext, context)
+    }
+}
+
