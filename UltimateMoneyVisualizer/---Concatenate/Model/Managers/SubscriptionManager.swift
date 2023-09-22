40988fc140fc10abe61fa4c5790df3010af1636b
vdeaugustine <32307897+vdeaugustine@users.noreply.github.com>
2023-09-22 00:31:19 -0700

    fixing most recent error for xcode cloud

--- UltimateMoneyVisualizer/---Concatenate/Model/Managers/SubscriptionManager.swift
+++ UltimateMoneyVisualizer/---Concatenate/Model/Managers/SubscriptionManager.swift
@@ -21,7 +21,11 @@ class SubscriptionManager: ObservableObject {
         // Call to your server, or Apple's servers to check the subscription status
         // You will typically want to cache the result to avoid frequent network calls
         // For simplicity, we're returning a constant value here
-        return false
+        
+        let answer = false
+        
+        isPremiumUser = answer
+        return answer
     }
 
     // Use this function to guard premium features
