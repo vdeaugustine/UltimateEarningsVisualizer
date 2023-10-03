e54cf117a50b9cd48505c66e731dfc19eeeda2ee
vdeaugustine <32307897+vdeaugustine@users.noreply.github.com>
2023-10-02 21:10:03 -0700

    i don't even know man

---
+++ UltimateMoneyVisualizer/EnterLumpSumView 3.swift
@@ -0,0 +1,128 @@
+//
+//  EnterLumpSumView.swift
+//  UltimateMoneyVisualizer
+//
+//  Created by Vincent DeAugustine on 9/28/23.
+//
+
+import SwiftUI
+
+// MARK: - EnterLumpSumView
+
+struct EnterLumpSumView: View {
+    @State private var amount: Double = 0
+    @State private var showSheet: Bool = false
+    @ObservedObject private var user: User = .main
+    @Environment(\.dismiss) private var dismiss
+    @State private var showErrorAlert = false
+    @State private var errorString: String = ""
+    @Environment(\.managedObjectContext) private var viewContext
+    @State private var isFromWorking = false
+    @State private var fromSavings: Double = 0
+    @State private var fromWages: Double = 0
+
+    @State private var savingsLimit: Double = 0
+    @State private var wagesLimit: Double = 0
+
+    var body: some View {
+        Form {
+            Section("Amount") {
+                Button {
+                    showSheet.toggle()
+                } label: {
+                    HStack {
+                        SystemImageWithFilledBackground(systemName: "dollarsign")
+                        Text(amount.simpleStr(2, false, useCommas: true))
+                            .font(.headline)
+                        Spacer()
+                        Text("Edit")
+                    }
+                }
+                .foregroundStyle(.black)
+            }
+
+            Section("Mark as earned wages") {
+                Toggle("Is from working", isOn: $isFromWorking)
+            }
+
+            Section("Source") {
+                DisclosureGroup {
+                    Slider(value: $fromSavings, in: 0 ... amount)
+
+                } label: {
+                    HStack {
+                        Text("From savings")
+                        Spacer()
+                        Text(fromSavings.money())
+                    }
+                }
+                .listRowSeparator(.hidden)
+
+                DisclosureGroup {
+                    Slider(value: $fromWages, in: 0 ... amount)
+
+                } label: {
+                    HStack {
+                        Text("From wages")
+                        Spacer()
+                        Text(fromWages.money())
+                    }
+                }
+                .listRowSeparator(.hidden)
+            }
+
+            Button("Save") {
+                do {
+                    if let bank = user.bank {
+                        bank.amount = amount
+                        bank.isFromWorking = isFromWorking
+                    } else {
+                        let bank = Bank(context: viewContext)
+                        bank.user = user
+                        bank.amount = amount
+                        bank.isFromWorking = isFromWorking
+                    }
+
+                    try viewContext.save()
+                    dismiss()
+                } catch {
+                    errorString = error.localizedDescription
+                    showErrorAlert = true
+                }
+            }
+        }
+        .sheet(isPresented: $showSheet, content: {
+            EnterDoubleView(dubToEdit: $amount, format: .dollar)
+        })
+        .onAppear {
+            guard let existing = user.bank else { return }
+            amount = existing.amount
+        }
+        .alert("Error saving amount", isPresented: $showErrorAlert) {} message: {
+            Text(errorString)
+        }
+        .putInTemplate(title: "Enter amount")
+        .onChange(of: fromSavings, perform: { value in
+            let availableForWages = amount - value
+            if availableForWages < 0 {
+                wagesLimit -= abs(availableForWages)
+                fromWages = abs(availableForWages)
+            }
+        })
+        .onChange(of: fromWages, perform: { value in
+            let availableForSavings = amount - value
+            if availableForSavings < 0 {
+                savingsLimit -= abs(availableForSavings)
+                fromSavings = availableForSavings
+            }
+        })
+        .onChange(of: amount, perform: { _ in
+            wagesLimit = amount - fromSavings
+            savingsLimit = amount - fromWages
+        })
+    }
+}
+
+#Preview {
+    EnterLumpSumView()
+}
---
+++ UltimateMoneyVisualizer/Views/Onboarding/WhatIsASavedItem 3.swift
@@ -0,0 +1,93 @@
+//
+//  WhatIsASavedItem.swift
+//  UltimateMoneyVisualizer
+//
+//  Created by Vincent DeAugustine on 8/29/23.
+//
+
+import SwiftUI
+
+// MARK: - WhatIsASavedItem
+
+struct WhatIsASavedItem: View {
+    @EnvironmentObject private var vm: OnboardingModel
+
+    var body: some View {
+        GeometryReader { geo in
+            VStack(spacing: 40) {
+                Text("A penny saved is a penny earned")
+                    .font(.system(.largeTitle, weight: .bold))
+                    .multilineTextAlignment(.center)
+                    .padding(.top, 30)
+                    .layoutPriority(2)
+                ScrollView {
+                    Image("saving")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(maxWidth: geo.size.width, maxHeight: 250)
+                        .layoutPriority(2)
+
+                    VStack {
+                        VStack(alignment: .leading, spacing: 20) {
+                            //                        VStack(alignment: .leading) {
+                            //                            HStack {
+                            //                                Circle().frame(width: 5)
+                            //                                Text("Make coffee at home instead of buying it?")
+                            //                            }
+                            //                            HStack {
+                            //                                Circle().frame(width: 5)
+                            //                                Text("Used a coupon at the grocery store?")
+                            //                            }
+                            //                            HStack {
+                            //                                Circle().frame(width: 5)
+                            //                                Text("Resisted a tempting purchase?")
+                            //                            }
+                            //
+                            //
+                            //                        }
+
+                            //                        Text("Record each moment and witness your savings grow steadily.")
+
+                            VStack(alignment: .leading) {
+                                Text("Any moment").font(.headline)
+
+                                Text("It could be as simple as making coffee at home instead of buying it on the way to work.")
+                            }
+
+                            VStack(alignment: .leading) {
+                                Text("Adds up quick").font(.headline)
+
+                                Text("While these moments might seem small, their collective impact is profound.")
+                            }
+
+                            VStack(alignment: .leading) {
+                                Text("Rewarding").font(.headline)
+                                Text("Recording them not only encourages mindful spending but also offers the satisfaction of directing those saved dollars towards achieving your goals.")
+                            }
+                        }
+                        .padding(.horizontal)
+                    }
+                    .scrollIndicators(.hidden)
+
+                    Spacer()
+
+                    OnboardingButton(title: "Record first Saved Item") {
+                        vm.increaseScreenNumber()
+                    }
+                    .padding(.horizontal, 30)
+                    .padding(.bottom, 50)
+                    .layoutPriority(0)
+                }
+            }
+        }
+    }
+}
+
+// MARK: - WhatIsASavedItem_Previews
+
+struct WhatIsASavedItem_Previews: PreviewProvider {
+    static var previews: some View {
+        WhatIsASavedItem()
+            .environmentObject(OnboardingModel())
+    }
+}
