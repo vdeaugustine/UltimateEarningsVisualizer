4dd86c9eab3735e36b019f31b37a5583ed71c8cf
vdeaugustine <32307897+vdeaugustine@users.noreply.github.com>
2023-10-03 19:34:08 -0700

    Improved create saved item view

--- UltimateMoneyVisualizer/---Concatenate/Model/Core Data/Extensions/ExtendSaved.swift
+++ UltimateMoneyVisualizer/---Concatenate/Model/Core Data/Extensions/ExtendSaved.swift
@@ -114,7 +114,7 @@ public extension Saved {
     var totalAvailable: Double {
         amount - totalAllocated
     }
-    
+
     var percentSpent: Double {
         (totalAllocated / amount).roundTo(places: 1)
     }
@@ -126,7 +126,7 @@ public extension Saved {
 
         return allocations
     }
-    
+
     func amountForAllInstances(user: User) -> Double {
         user.getInstancesOf(savedItem: self)
             .reduce(Double.zero) { partialResult, saved in
@@ -171,5 +171,65 @@ public extension Saved {
                   context: context)
 
         try context.save()
+    }
+
+    static func getExampleTitleAndDescription() -> (title: String, info: String) {
+        let moneySavingIdeasInfo = ["Skipped buying coffee",
+                                    "Made lunch at home",
+                                    "Used public transportation instead of driving",
+                                    "Canceled unused subscriptions",
+                                    "Bought generic brands instead of name brands",
+                                    "Brought reusable water bottle instead of buying bottled water",
+                                    "Took advantage of discounts and coupons",
+                                    "Switched to energy-efficient light bulbs",
+                                    "Used a programmable thermostat to save on heating and cooling costs",
+                                    "Opted for DIY home repairs and maintenance",
+                                    "Cut down on dining out",
+                                    "Sold items you no longer need",
+                                    "Implemented a budget and tracked expenses",
+                                    "Used cashback and rewards credit cards",
+                                    "Shopped during sales and clearance events",
+                                    "Reduced impulse purchases",
+                                    "Cut down on unused gym memberships or services",
+                                    "Biked or walked instead of using a car for short trips",
+                                    "Switched to a more affordable phone plan",
+                                    "Switched to a cheaper internet or cable TV plan",
+                                    "Installed energy-efficient appliances",
+                                    "Participated in clothing swaps with friends",
+                                    "Reused and repurposed items instead of buying new",
+                                    "Started a side hustle or freelance work",
+                                    "Participated in community events and free activities",
+                                    "Took advantage of employer benefits and discounts"]
+
+        let moneySavingIdeasTitle = ["Instead of buying at Starbucks",
+                                     "Instead of going out to eaat",
+                                     "Use public transportation",
+                                     "Cancel unused subscriptions",
+                                     "Buy generic brands",
+                                     "Use reusable water bottle",
+                                     "Apply discounts and coupons",
+                                     "Switch to LED bulbs",
+                                     "Programmable thermostat",
+                                     "Try DIY home repairs",
+                                     "Cut down dining out",
+                                     "Sell unused items",
+                                     "Budget and track expenses",
+                                     "Cashback credit cards",
+                                     "Shop during sales",
+                                     "Reduce impulse buys",
+                                     "Avoid unused gym memberships",
+                                     "Walk or bike short trips",
+                                     "Affordable phone plan",
+                                     "Cheaper internet or cable",
+                                     "Energy-efficient appliances",
+                                     "Clothing swaps with friends",
+                                     "Reuse and repurpose items",
+                                     "Start a side hustle",
+                                     "Attend free community events",
+                                     "Utilize employer discounts"]
+
+        let int = Int.random(in: 0 ..< moneySavingIdeasInfo.count)
+        return (title: moneySavingIdeasTitle.safeGet(at: int) ?? "Skip buying coffee",
+                info: moneySavingIdeasInfo.safeGet(at: int) ?? "Instead of buying at Starbucks")
     }
 }
