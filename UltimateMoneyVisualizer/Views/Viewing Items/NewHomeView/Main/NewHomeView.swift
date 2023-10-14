ea10f66ca36ef7ea7dae153bc218a008f1b8e59f
vdeaugustine <32307897+vdeaugustine@users.noreply.github.com>
2023-10-14 14:21:43 -0700

    home view walkthrough done

--- UltimateMoneyVisualizer/Views/Viewing Items/NewHomeView/Main/NewHomeView.swift
+++ UltimateMoneyVisualizer/Views/Viewing Items/NewHomeView/Main/NewHomeView.swift
@@ -1,10 +1,10 @@
+import PopupView
+import ScrollKit
 import SwiftUI
 
 extension ScrollViewProxy {
-    func scrollWithAnimation<ID>(
-        _ id: ID,
-        anchor: UnitPoint? = nil
-    ) where ID : Hashable {
+    func scrollWithAnimation<ID>(_ id: ID,
+                                 anchor: UnitPoint? = nil) where ID: Hashable {
         withAnimation {
             self.scrollTo(id, anchor: anchor)
         }
@@ -16,75 +16,119 @@ extension ScrollViewProxy {
 struct NewHomeView: View {
     @StateObject private var vm: NewHomeViewModel = .shared
     @ObservedObject private var settings = User.main.getSettings()
+    @State private var scrollOffset: CGPoint = .zero
 
-    @State private var showWageBreakdownPopover = true
+    @State private var showWageBreakdownPopover = false
+    @State private var summaryPopover = false
     @State private var moreStatsPopover = false
     @State private var totalsPopover = false
     @State private var netMoneyPopover = false
     @State private var payoffQueuePopover = false
+    @State private var showFirstPopOverMain = false
+    @State private var wagePopover = false
+    @State private var timeBlockPopover = false
+    @State private var quickAddPopover = false
+
+    @State private var scrollPosition = CGFloat.zero
 
     enum ViewTags: Hashable {
-        case totals, summary, netMoney, payoffQueue, wageBreakdown, timeBlocks
+        case totals, summary, netMoney, payoffQueue, wageBreakdown, timeBlocks, scrollView
+    }
+
+    func handleOffset(_ scrollOffset: CGPoint) {
+        self.scrollOffset = scrollOffset
+        print("New offset: ", scrollOffset)
     }
 
     var body: some View {
         ScrollViewReader { scrollProxy in
-            ScrollView {
-                VStack(spacing: 40) {
-                    TotalsToDate_HomeView()
-                    SummaryView_HomeView()
-                    NetMoney_HomeView()
-                        .defaultPopover(isPresented: $netMoneyPopover, text: "This graph shows your net money over time.\nIt takes into account your earnings + saved\nitems and your payoff items", direction: .down)
-                    PayoffQueueView_HomeView()
-                        .id(ViewTags.payoffQueue)
-                        .floatingPopover(isPresented: $payoffQueuePopover, arrowDirection: .up) {
-                            
-                            VStack(alignment: .leading, spacing: 20) {
-                                
-                                Group {
-                                    Text("Your Payoff Queue automatically utilizes every penny you earn in real-time to chip away at your expenses and goals!")
-                                    Text("As you earn, watch your queued items get paid off, one by one, keeping your financial journey smoothly on track.")
-                                    Text("Simply prioritize, and your earnings handle the rest!")
+            GeometryReader { geo in
+                ScrollViewWithOffset(onScroll: handleOffset) {
+                    Color.clear.frame(height: 0)
+                        .id(ViewTags.scrollView)
+                    VStack(spacing: 40) {
+                        TotalsToDate_HomeView()
+                            .id(ViewTags.totals)
+                            .floatingPopover(isPresented: $totalsPopover, arrowDirection: .up) {
+                                Totals_HomeView_Popover {
+                                    totalsPopover = false
+                                    scrollProxy.scrollWithAnimation(ViewTags.netMoney, anchor: .bottom)
+                                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                        netMoneyPopover = true
+                                    }
                                 }
-                                .font(.subheadline)
-                                    
-                                Button("Next") {
-                                    
+                            }
+                        SummaryView_HomeView()
+                            .id(ViewTags.summary)
+                        NetMoney_HomeView()
+                            .id(ViewTags.netMoney)
+                            .floatingPopover(isPresented: $netMoneyPopover, arrowDirection: .down) {
+                                NetMoney_HomeView_Popover {
+                                    netMoneyPopover = false
+                                    scrollProxy.scrollWithAnimation(ViewTags.payoffQueue, anchor: .top)
+                                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                        payoffQueuePopover = true
+                                    }
                                 }
-                                .frame(maxWidth: .infinity, alignment: .bottomTrailing)
-                                    
-                                
                             }
-                            .padding()
-                            .foregroundStyle(.white)
-                            .frame(height: 275)
-                            .background {
-                                
-                                settings.themeColor.getGradient()
-                                    .clipShape(RoundedRectangle(cornerRadius: 10))
-                                    .padding(-20)
+                        PayoffQueueView_HomeView()
+                            .id(ViewTags.payoffQueue)
+                            .floatingPopover(isPresented: $payoffQueuePopover,
+                                             arrowDirection: .up) {
+                                PayoffQueue_HomeView_Popup {
+                                    payoffQueuePopover = false
+                                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                        wagePopover = true
+                                    }
+                                }
                             }
-                            
-                            
-                            
-                        }
 
-                    WageBreakdown_HomeView()
+                        WageBreakdown_HomeView()
+                            .id(ViewTags.wageBreakdown)
+                            .floatingPopover(isPresented: $wagePopover, arrowDirection: .down) {
+                                WageBreakdown_HomeView_Popover {
+                                    wagePopover = false
+                                    scrollProxy.scrollWithAnimation(ViewTags.timeBlocks, anchor: .bottom)
+                                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                        timeBlockPopover = true
+                                    }
+                                }
+                            }
 
-                    TopTimeBlocks_HomeView()
-                }
-                .padding(.top)
-                .onAppear(perform: {
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                        scrollProxy.scrollWithAnimation(ViewTags.payoffQueue, anchor: .top)
-                        
-                        payoffQueuePopover = true
+                        TopTimeBlocks_HomeView()
+                            .id(ViewTags.timeBlocks)
+                            .floatingPopover(isPresented: $timeBlockPopover, arrowDirection: .down) {
+                                TimeBlock_HomeView_Popover {
+                                    timeBlockPopover = false
+                                    scrollProxy.scrollWithAnimation(ViewTags.scrollView, anchor: .zero)
+                                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                        quickAddPopover = true
+                                    }
+                                }
+                            }
                     }
-                })
-//
+                    .padding(.top)
+
+                    .onAppear(perform: {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                            totalsPopover = true
+                        }
+                    })
+                }
+                .popup(isPresented: $showFirstPopOverMain) {
+                    VStack {
+                        Text("Welcome to home view")
+                    }
+                    .frame(maxWidth: geo.size.width - 50, maxHeight: geo.size.height - 140)
+                    .padding()
+                    .background {
+                        Color.white
+                            .clipShape(RoundedRectangle(cornerRadius: 20))
+                            .shadow(radius: 3)
+                    }
+                }
             }
         }
-
         .blur(radius: vm.quickMenuOpen ? 3 : 0)
         .overlay {
             if vm.quickMenuOpen {
@@ -97,10 +141,21 @@ struct NewHomeView: View {
         }
         .animation(/*@START_MENU_TOKEN@*/ .easeIn/*@END_MENU_TOKEN@*/, value: vm.quickMenuOpen)
         .safeAreaInset(edge: .bottom) {
-            HStack {
-                Spacer()
-                FloatingPlusButton(isShowing: $vm.quickMenuOpen)
-                    .padding(.bottom)
+            if !totalsPopover,
+               !netMoneyPopover,
+               !payoffQueuePopover,
+               !wagePopover,
+               !timeBlockPopover {
+                HStack {
+                    Spacer()
+                    FloatingPlusButton(isShowing: $vm.quickMenuOpen)
+                        .padding(.bottom)
+                        .floatingPopover(isPresented: $quickAddPopover, arrowDirection: .down) {
+                            QuickAddButton_HomeView_Popover {
+                                quickAddPopover = false
+                            }
+                        }
+                }
             }
         }
         .environmentObject(vm)
